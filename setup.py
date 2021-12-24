7561c75e2a28bccd1d8e5ccd696f497ec1a2cdea
W. Aaron Morris <waaronmorris@gmail.com>
2021-12-24 10:18:04 -0800

    Publish if Tagged

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=getenv('VERSION_NUMBER', '0.0.1'),
+    version=getenv('GITHUB_REF_NAME', '0.0.1.1'),
 
     description='A simple package for scraping baseball data from the most popular sites.',
     long_description=long_description,
